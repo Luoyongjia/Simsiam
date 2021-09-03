1005f241efc5cbe68ea0c5c7bea38e2ab0dfa513
Luoyongjia <luoyongjiavb@gmail.com>
2021-09-03 21:13:51 +0800

    Finished parser, dataloader.

---
+++ Utils/parser.py
@@ -0,0 +1,101 @@
+import argparse
+import os
+
+import numpy as np
+import torch
+import random
+
+import re
+import yaml
+
+import shutil
+
+from datetime import datetime
+
+
+class Namespace(object):
+    """
+    load configs from xxx.yaml
+    """
+    def __init__(self, Dict):
+        for key, value in Dict.items():
+            assert isinstance(key, str) and re.match("[A-Za-z_-]", key)
+            if isinstance(value, dict):
+                self.__dict__[key] = Namespace(value)
+            else:
+                self.__dict__[key] = value
+
+    def __getattr__(self, attribute):
+        raise AttributeError(f"Can not find {attribute} in namespace. Please write {attribute} in config file!")
+
+
+def set_deterministic(seed):
+    """default is None"""
+    if seed is not None:
+        print(f"Deterministic with seed = {seed}")
+        random.seed(seed)
+        np.random.seed(seed)
+        torch.manual_seed(seed)
+        torch.cuda.manual_seed(seed)
+        torch.backends.cudnn.deterministic = True
+        torch.backends.cudnn.benchmark = False
+
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-c', '--config-file', type=str, default="/Users/luoyongjia/Program/py/SimSiam/configs/simsiam_cifar.yaml", help="xxx.yaml")
+    parser.add_argument('--debug', action='store_true', default=False)
+    parser.add_argument('--debug_subset_size', type=int, default=8)
+    parser.add_argument('--download', action='store_true', help="if cannot fine dataset, download from web")
+    parser.add_argument('--data_dir', type=str, default="/Users/luoyongjia/Research/Data/cifar10")
+    parser.add_argument('--log_dir', type=str, default="/Users/luoyongjia/Program/py/SimSiam/res/logs")
+    parser.add_argument('--checkpoint_dir', type=str, default="/Users/luoyongjia/Program/py/SimSiam/res/checkpoints")
+    parser.add_argument('--device', type=str, default='cuda' if torch.cuda.is_available() else 'cpu')
+    parser.add_argument('--eval_from', type=str, default=None)
+
+    args = parser.parse_args()
+
+    with open(args.config_file, 'r') as f:
+        for key, value in Namespace(yaml.load(f, Loader=yaml.FullLoader)).__dict__.items():
+            vars(args)[key] = value
+
+    if args.debug:
+        if args.train:
+            args.train.batch_size = 2
+            args.train.num_epochs = 1
+            args.train.stop_at_epoch = 1
+        if args.eval:
+            args.eval.batch_size = 2
+            args.eval.num_epochs = 1
+        args.dataset.num_workers = 0
+
+    assert not None in [args.log_dir, args.data_dir, args.name]
+
+    args.log_dir = os.path.join(args.log_dir, 'progress_' + datetime.now().strftime('%m%d%H%M%S_')+args.name)
+    os.makedirs(args.log_dir, exist_ok=False)
+    print(f'Creating file {args.log_dir}')
+    os.makedirs(args.checkpoint_dir, exist_ok=True)
+
+    shutil.copy2(args.config_file, args.log_dir)
+    set_deterministic(args.seed)
+
+    vars(args)['aug_kwargs'] = {
+        'image_size': args.dataset.image_size,
+    }
+    vars(args)['dataset_kwargs'] = {
+        'dataset': args.dataset.name,
+        'data_dir': args.data_dir,
+        'download': args.download,
+        'debug_subset_size': args.debug_subset_size if args.debug else None,
+    }
+    vars(args)['dataloader_kwargs'] = {
+        'drop_last': True,
+        'pin_memory': True,
+        'num_workers': args.dataset.num_workers,
+    }
+
+    return args
+
+
+if __name__ == "__main__":
+    args = get_args()
---
+++ dataLoader.py
@@ -0,0 +1,121 @@
+import random
+
+import torch
+import torchvision
+from torchvision import transforms
+from PIL import Image, ImageFilter
+
+from Utils.parser import get_args
+
+IMAGENETNORM = [[0.485, 0.456, 0.406],[0.229, 0.224, 0.225]]
+CIFAR10NORM = [[0.4914, 0.4822, 0.4465], [0.2023, 0.1994, 0.2010]]
+
+
+def get_dataset(dataset, data_dir, transform, train=True, download=False, debug_subset_size=None):
+    if dataset == 'cifar10':
+        dataset = torchvision.datasets.CIFAR10(data_dir, train=train, transform=transform, download=download)
+    elif dataset == 'cifar100':
+        dataset = torchvision.datasets.CIFAR100(data_dir, train=train, transform=transform, download=download)
+    elif dataset == 'imagenet':
+        dataset = torchvision.datasets.ImageNet(data_dir, split='train' if train is True else 'val',
+                                                transform=transform, download=download)
+    else:
+        raise NotImplementedError
+
+    if debug_subset_size is not None:
+        dataset = torch.utils.data.Subset(dataset, range(0, debug_subset_size))
+        dataset.classes = dataset.dataset.classe
+        dataset.targets = dataset.dataset.targets
+
+    return dataset
+
+
+class GaussianBlur(object):
+    """from SimCLR."""
+    def __init__(self, sigma=[.1, 2.]):
+        self.sigma = sigma
+
+    def __call__(self, x):
+        sigma = random.uniform(self.sigma[0], self.sigma[1])
+        x = x.filter(filter(ImageFilter.GaussianBlur(radius=sigma)))
+        return x
+
+
+class transform_single():
+    def __init__(self, image_size, train, normalize=CIFAR10NORM):
+        if train is True:
+            self.transform = transforms.Compose([
+                transforms.RandomResizedCrop(image_size, scale=(0.08, 1.0), ratio=(3.0/4.0, 4.0/3.0), interpolation=Image.BICUBIC),
+                transforms.RandomHorizontalFlip(),
+                transforms.ToTensor(),
+                transforms.Normalize(*normalize)
+            ])
+        else:
+            self.transform = transforms.Compose([
+                transforms.Resize(int(image_size*(8/7)), interpolation=Image.BICUBIC),
+                transforms.CenterCrop(image_size),
+                transforms.ToTensor(),
+                transforms.Normalize(*normalize)
+            ])
+
+    def __call__(self, x):
+        return self.transform(x)
+
+
+class transform_simsiam():
+    def __init__(self, image_size, normalize = CIFAR10NORM):
+        image_size = 224 if image_size is None else image_size
+        p_blur = 0.5 if image_size > 32 else 0
+        self.transform = transforms.Compose([
+            transforms.RandomResizedCrop(image_size, scale=(0.2, 1.0)),
+            transforms.RandomHorizontalFlip(),
+            transforms.RandomApply([transforms.ColorJitter(0.4, 0.4, 0.4, 0.1)], p=0.8),
+            transforms.RandomGrayscale(p=0.2),
+            transforms.RandomApply([GaussianBlur([.1, 2.])], p=0.5),
+            transforms.ToTensor(),
+            transforms.Normalize(*normalize)
+        ])
+
+    def __call__(self, x):
+        x1 = self.transform(x)
+        x2 = self.transform(x)
+        return x1, x2
+
+
+def get_transform(image_size=224, train=True, train_classifier=None):
+    if train is True:
+        augmentation = transform_simsiam(image_size)
+    elif train is False:
+        if train_classifier is None:
+            raise Exception
+        augmentation = transform_single(image_size, train = train_classifier)
+    else:
+        raise NotImplementedError
+
+    return augmentation
+
+
+def load_data(args):
+    train_dataset = get_dataset(transform=get_transform(train=True, **args.aug_kwargs),
+                                train=True,
+                                **args.dataset_kwargs)
+    memo_dataset = get_dataset(transform=get_transform(train=False, train_classifier=False, **args.aug_kwargs),
+                               train=True,
+                               **args.dataset_kwargs)
+    test_dataset = get_dataset(transform=get_transform(train=False, train_classifier=False, **args.aug_kwargs),
+                               train=False,
+                               **args.dataset_kwargs)
+
+    train_loader = torch.utils.data.DataLoader(train_dataset, shuffle=True, batch_size=args.train.batch_size,
+                                               **args.dataloader_kwargs)
+    memo_loader = torch.utils.data.DataLoader(memo_dataset, shuffle=False, batch_size=args.train.batch_size,
+                                             **args.dataloader_kwargs)
+    test_loader = torch.utils.data.DataLoader(test_dataset, shuffle=False, batch_size=256,
+                                              **args.dataloader_kwargs)
+
+    return train_loader, memo_loader, test_loader
+
+
+if __name__ == "__main__":
+    args = get_args()
+    train_loader, _, _ = load_data(args)
